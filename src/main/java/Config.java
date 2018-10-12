72d3cec9261198ea214f30acf7ec015df7d6b9ff
KarlJosephKumar <kjkumr@gmail.com>
2018-10-12 21:31:32 +0300

    added config, database to the point where groups and student group is missing

---
+++ src/main/java/Config.java
@@ -0,0 +1,20 @@
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Config {
+    static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory(){
+        if (sessionFactory != null)
+            return sessionFactory;
+
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        MetadataSources sources = new MetadataSources(registry);
+        Metadata metadata = sources.getMetadataBuilder().build();
+        sessionFactory = metadata.getSessionFactoryBuilder().build();
+        return sessionFactory;
+    }
+}
