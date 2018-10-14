698badd72cf6719dfd696ad4c2ed064e6340b547
dinirichard <dinirichard@hotmail.com>
2018-10-14 17:09:50 +0300

    Complete SQL script for database creation Completed Added procedures for inserting into tables

--- src/main/java/sql_scripts/hibernate_project.sql
+++ src/main/java/sql_scripts/hibernate_project.sql
@@ -68,12 +68,13 @@ create procedure insert_student(
     insert into student (name, surname, date_of_birth, nationality) values (p_name, p_surname, p_date_of_birth, p_national);
   end;
 
-call insert_student('Victor', 'Vitya', '1990-10-13', 'ukrainian');
-call insert_student('Alo', 'Ano', '1990-12-25', 'Estonian');
-call insert_student('Andy', 'Kwame', '1993-11-13', 'Ghanaian');
-call insert_student('Daniel', 'Richard', '1990-5-13', 'Nigerian');
-call insert_student('Karl', 'Joseph', '1990-8-13', 'Estonian');
-call insert_student('Krislin', 'aaaaa', '1990-2-13', 'Estonian');
+call insert_student('Ryan', 'Alex', '1990-10-13', 'British');
+call insert_student('Raquel', 'Goncalves', '1990-12-25', 'Portuguese');
+call insert_student('Sander', 'Aa', '1993-11-13', 'Estonian');
+call insert_student('Oskar', 'Lin', '1990-5-13', 'Swedish');
+call insert_student('Aleks', 'Sander', '1990-8-13', 'Estonian');
+call insert_student('Karolina', 'Kowski', '1990-2-13', 'Estonian');
+call insert_student('Denis', 'Senko', '1990-2-13', 'Russian');
 
 
 --FOR INSERTING ADDRESSES
@@ -133,7 +134,7 @@ create procedure insert_weekday_schedule(
   p_friday varchar(45)
 )
   begin
-    insert into schedule (monday, tuesday, wednesday, thurday, friday) values (p_monday, p_tuesday, p_wednesday, p_thurday, p_friday);
+    insert into schedule (monday, tuesday, wednesday, thursday, friday) values (p_monday, p_tuesday, p_wednesday, p_thurday, p_friday);
   end;
 
   --WEEKEND SCHEDULE---
@@ -144,3 +145,56 @@ create procedure insert_weekend_schedule(
   begin
     insert into schedule (saturday, sunday) values (p_saturday, p_sunday);
   end;
+
+call insert_weekday_schedule('18:30-21:30', '18:30-21:30', '18:30-21:30', '18:30-21:30', '18:30-21:30');
+call insert_weekend_schedule ('9:00-17:00', '9:00-17:00');
+
+
+----for Inserting CLASS----------
+create procedure insert_class(
+  p_schedule_id     int,
+  p_room_id  int,
+  p_date_start DATETIME,
+  p_date_finish DATETIME,
+  p_course_id  int
+)
+  begin
+    insert into class (schedule_id, room_id, date_start, date_finish, course_id) values (p_schedule_id, p_room_id, p_date_start, p_date_finish, p_course_id);
+  end;
+
+  call insert_class(1, 1, date_sub(NOW(), interval 3 MONTH), DATE_ADD(NOW(), interval 3 MONTH), 1);
+  call insert_class(2, 2, NOW(), DATE_ADD(NOW(), interval 6 MONTH), 2);
+
+  -----for Inserting STUDENT_CLASS-----------
+create procedure insert_student_class(
+  p_student_id int,
+  p_class_id int
+)
+  begin
+    insert into student_class (student_id, class_id) values (p_student_id, p_class_id);
+  end;
+
+  call insert_student_class(1,1);
+  call insert_student_class(4,1);
+  call insert_student_class(5,1);
+  call insert_student_class(6,1);
+  call insert_student_class(7,1);
+  call insert_student_class(8,1);
+  call insert_student_class(9,1);
+  call insert_student_class(10,1);
+  call insert_student_class(11,1);
+  call insert_student_class(12,1);
+  call insert_student_class(13,1);
+
+  -----for Inserting PAYMENTS-----------
+  create procedure insert_payments(
+  p_student_id int,
+  p_course_id int,
+  p_invoice_value float(12)
+)
+  begin
+    insert into payments (student_id, course_id, invoice_value) values (p_student_id, p_course_id, p_invoice_value);
+  end;
+
+call insert_payments(7, 1, 1800.00);
+
