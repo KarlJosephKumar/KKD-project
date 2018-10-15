0aad07e3b63181b710e1d34730ecfe4775f4b5c9
KarlJosephKumar <kjkumr@gmail.com>
2018-10-15 18:42:05 +0300

    config, tables done

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,8 +1,8 @@
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+        import org.hibernate.boot.Metadata;
+        import org.hibernate.boot.MetadataSources;
+        import org.hibernate.boot.registry.StandardServiceRegistry;
+        import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class Config {
     static SessionFactory sessionFactory;
