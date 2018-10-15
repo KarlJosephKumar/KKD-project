48080fa498a3220ddf3dfebcc1bafbc2f24187db
KarlJosephKumar <kjkumr@gmail.com>
2018-10-15 20:52:49 +0300

    config, tables done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,24 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 public class Main {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = Config.getSessionFactory();
+        Session session =  sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Room room = session
+                .createNamedQuery("get_all_by_id", Room.class)
+                .setParameter("id", 1)
+                .getSingleResult();
+
+        System.out.println(room);
+
+
+
+        transaction.commit();
+        session.close();
+    }
 
 }
