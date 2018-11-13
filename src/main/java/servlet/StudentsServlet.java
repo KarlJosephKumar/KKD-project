3f76fdd0c93db0b6e44e58cbb9c2662a82c5552b
dinirichard <dinirichard@hotmail.com>
2018-11-13 21:30:34 +0200

    created session_login table

--- src/main/java/servlet/StudentsServlet.java
+++ src/main/java/servlet/StudentsServlet.java
@@ -1,15 +1,42 @@
 package servlet;
 
+import hibernate.Student;
+import model.HibernateMethods;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class StudentsServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        HibernateMethods hibernateMethods = HibernateMethods.getInstance();
+
+        List<Student> studentList = hibernateMethods.getAllStudents();
+        List<String> fullname = new ArrayList<>();
+        List<Date> dob = new ArrayList<>();
+        List<String> nationality = new ArrayList<>();
+
+        //Get students full name
+        for (Student student : studentList) {
+            fullname.add(student.getSurname() + " " + student.getName());
+            dob.add(student.getDate_of_birth());
+            nationality.add(student.getNationality());
+        }
+
+        req.setAttribute("studentList", studentList);
+
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/views/StudentsView.jsp");
+        requestDispatcher.forward(req, resp);
+
 
     }
 }
--- src/main/webapp/views/StudentsView.jsp
+++ src/main/webapp/views/StudentsView.jsp
@@ -1,4 +1,5 @@
-<%--
+<%@ page import="java.util.List" %>
+<%@ page import="hibernate.Student" %><%--
   Created by IntelliJ IDEA.
   User: dinir
   Date: 13-Nov-18
@@ -8,9 +9,24 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>Students List</title>
 </head>
 <body>
+<table style="width: 100%">
+    <tr>
+        <th>Fullname</th>
+        <th>Date of Birth</th>
+        <th>Nationality</th>
+    </tr>
+
+<%
+    List<Student> studentList = (List<Student>) request.getAttribute("studentList");
+    for (Student student : studentList) {
+        out.println("<tr><td>" + student.getName() + " " + student.getSurname() + "</td><td>" + student.getDate_of_birth() + "</td><td>" + student.getNationality() + "</td></tr>");
+    }
+%>
+
+
 
 </body>
 </html>
