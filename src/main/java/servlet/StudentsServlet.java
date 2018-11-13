956b4759d9a5ae34a0abda4fd6aaec783b1f7ace
dinirichard <dinirichard@hotmail.com>
2018-11-13 19:51:56 +0200

    created session_login table

--- src/main/java/servlet/StudentsServlet.java
+++ src/main/java/servlet/StudentsServlet.java
@@ -1,4 +1,15 @@
 package servlet;
 
-public class StudentsServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class StudentsServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
